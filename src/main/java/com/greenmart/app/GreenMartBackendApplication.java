75f2a71fde9faf8eff85a2b9cb8511fb721237f8
Yash Verma <yashv521@gmail.com>
2025-01-29 17:54:13 +0530

    restructuring

--- src/main/java/com/greenmart/app/GreenMartBackendApplication.java
+++ src/main/java/com/greenmart/app/GreenMartBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GreenMartApplication {
+public class GreenMartBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GreenMartApplication.class, args);
+		SpringApplication.run(GreenMartBackendApplication.class, args);
 	}
 
 }
